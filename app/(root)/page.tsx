183d96e7817822aad3dd43cbe3a08dece48a2a35
OlatunjiAbiodun <olatunjia889@gmail.com>
2024-05-25 19:42:11 +0100

    add header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import React from "react"
 
 const Home = () => {
+  const loggenIn = {firstName:'Abiodun'};
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header"> 
+        <HeaderBox 
+        type="greeting"
+        title="Welcome"
+        user={loggenIn?.firstName || 'Guest'}
+        subtext="Access and manage your accounts and transcations efficiently."
+        />
+         </header>
+      </div>
+    </section>
   )
 }
 
